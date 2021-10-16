5c5b00120dac8bc151784abce0b4581b9ca43dbd
NachoDaibes <nachodaibes25@gmail.com>
2021-10-16 12:40:54 -0300

    pom sin JUnit

--- src/main/java/com/example/testingCarrito/testingCarrito.java
+++ src/main/java/com/example/testingCarrito/testingCarrito.java
@@ -4,8 +4,7 @@ package com.example.testingCarrito;
 public class testingCarrito {
     public static void main(String[] args) {
 
-        Producto producto= new Producto(10,"Tablet Samsung A50",
-                                        "jsahdbfjasd", 200 );
+        Producto producto= new Producto(10,"Tablet Samsung A50","jsahdbfjasd", 200 );
 
         Carrito carrito = new Carrito();
 
